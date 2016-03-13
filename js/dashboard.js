2775666f94a32b0d4ea490ebe5a93697391b2d02
Leo Brescia <lleobrescia@gmail.com>
2016-03-13 11:56:17 -0300

    Menu desktop

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,64 @@
- angular.module("dashboard", ['ngRoute']);
+angular.module("dashboard", ['ngRoute']);
+
+
+
+
+angular.module('dashboard')
+//cria o formato dos itens do menu
+.directive("navigation", [function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      menu: '='
+    },
+    templateUrl: '/dashboard/templates/navigation.html'
+  };
+
+}])
+//lista do menu
+.controller('sidebar', ['$scope','$location',function ($scope,$location) {
+
+  $scope.isActive = function (viewLocation) {
+    return viewLocation === $location.path();
+  };
+
+  $scope.menu =
+  [
+    {
+      Id: 1,
+      Name: "Dados do Casal",
+      url:'dados-do-casal'
+    },
+    {
+      Id: 2,
+      Name: 'Configurar Convite',
+      url:'configurar-convite'
+    },
+    {
+      Id: 3,
+      Name: 'Configurar Evento',
+      url:'configurar-evento'
+    },
+    {
+      Id: 4,
+      Name: 'Cadastrar Convidados',
+      url:'cadastrar-convidados'
+    },
+    {
+      Id: 5,
+      Name: 'Save the Date',
+      url:'save-the-date'
+    },
+    {
+      Id: 6,
+      Name: 'Enviar Convite',
+      url:'enviar-convite'
+    },
+    {
+      Id: 7,
+      Name: 'Convidados Confirmados',
+      url:'convidados-confirados'
+    }
+  ];
+}]);
--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,32 @@
- angular.module("dashboard").config(function($routeProvider){
-
-  //  $routeProvider.when('/contato',{
-  //    templateUrl:"/templates/page"
-  //  })
-  //  .otherwise({ redirectTo:'/'});
- });
-
+// Navegação
+angular.module("dashboard").config(function($routeProvider){
+  $routeProvider
+  .when('/',{
+    redirectTo:"/dados-do-casal"
+  })
+  .when('/dados-do-casal',{
+    templateUrl:"/dashboard/templates/page/dados_casal.html"
+  })
+  .when('/configurar-convite',{
+    templateUrl:"/dashboard/templates/page/configurar_convite.html"
+  })
+  .when('/configurar-evento',{
+    templateUrl:"/dashboard/templates/page/configurar_evento.html"
+  })
+  .when('/cadastrar-convidados',{
+    templateUrl:"/dashboard/templates/page/cadastrar_convidados.html"
+  })
+  .when('/save-the-date',{
+    templateUrl:"/dashboard/templates/page/save_the_date.html"
+  })
+  .when('/enviar-convite',{
+    templateUrl:"/dashboard/templates/page/enviar_convite.html"
+  })
+  .when('/convidados-confirados',{
+    templateUrl:"/dashboard/templates/page/convidados_confirmados.html"
+  })
+  .otherwise({ redirectTo:'/dados-do-casal'});
+});
 
  // angular.module('NoteWrangler', ['ngRoute'])
  // .config(['$routeProvider', function($routeProvider) {
