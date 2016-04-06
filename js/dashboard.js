e08b6c544b9436c4dcdf8699b3966a233da801ff
Leo Brescia <lleobrescia@gmail.com>
2016-04-06 10:28:51 -0300

    remocao das directives para um arquivo separado

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,30 +1,6 @@
 // INIT
 angular.module("dashboard", ['ngRoute', 'ngFileUpload', 'ngMask', 'rzModule', 'ngAnimate', 'ui.bootstrap']);
 
-//directive
-angular.module('dashboard').directive("navigation", [function() {
-  return {
-    restrict: 'E',
-    replace: true,
-    scope: {
-      menu: '='
-    },
-    templateUrl: '/dashboard/templates/navigation.html'
-  };
-}]);
-angular.module('dashboard').directive('onFinishRender', function($timeout) {
-  return {
-    restrict: 'A',
-    link: function(scope, element, attr) {
-      if (scope.$last === true) {
-        $timeout(function() {
-          scope.$emit('ngRepeatFinished');
-        });
-      }
-    }
-  }
-});
-
 //Controllers
 angular.module('dashboard').controller('sidebar', ['$scope', '$location', function($scope, $location) {
 
