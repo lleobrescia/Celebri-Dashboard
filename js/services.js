cc1b5f530b6a64b8657c4c5c13708733d60d6991
Leo Brescia <leo.brescia@pixla.com.br>
2016-05-16 01:41:18 -0300

    serviço save the date

--- js/services.js
+++ js/services.js
@@ -519,7 +519,53 @@ angular.module("dashboard").factory('PresentesLuaDeMel', ['CallAjax', '$q', func
     });
     return deferred.promise;
   };
+  var setData = function (xmlVar) {
+    var urlVar = "http://23.238.16.114/celebri/ServiceCasamento.svc/ConfigurarPresentesEscolhidos";
+    var call = CallAjax.resposta(urlVar, xmlVar);
+    var deferred = $q.defer();
+
+    call.success(function (data) {
+      deferred.resolve(data);
+    }).error(function () {
+      deferred.reject(arguments);
+    });
+    return deferred.promise;
+  };
   return {
     getData: getData,
+    setData:setData
+  };
+}]);
+
+angular.module("dashboard").factory('SaveTheDate', ['CallAjax', '$q', function (CallAjax, $q) {
+  var getData = function (id) {
+    var urlVar = "http://23.238.16.114/celebri/ServiceCasamento.svc/RetornarFormatacaoSaveTheDate";
+    var xmlVar = '<IdentificaocaoCasal xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Id_casal>' + id + '</Id_casal></IdentificaocaoCasal>';
+
+    var call = CallAjax.resposta(urlVar, xmlVar);
+    var deferred = $q.defer();
+
+    call.success(function (data) {
+      deferred.resolve(data);
+    }).error(function () {
+      deferred.reject(arguments);
+    });
+    return deferred.promise;
+  };
+  var setData = function (xmlVar) {
+    var urlVar = "http://23.238.16.114/celebri/ServiceCasamento.svc/FormatacaoSaveTheDate";
+    var call = CallAjax.resposta(urlVar, xmlVar);
+    var deferred = $q.defer();
+
+    call.success(function (data) {
+      deferred.resolve(data);
+    }).error(function () {
+      deferred.reject(arguments);
+    });
+    return deferred.promise;
+  };
+  return {
+    getData: getData,
+    setData:setData
   };
 }]);
