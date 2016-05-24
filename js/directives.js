6a8abe5e59cef73289b9c05280f87d4cd318b290
Leo Brescia <leo.brescia@pixla.com.br>
2016-05-24 16:22:45 -0300

    novo filtro

--- js/directives.js
+++ js/directives.js
@@ -1,4 +1,4 @@
-angular.module('dashboard').directive("navigation", [function() {
+angular.module('dashboard').directive("navigation", [function () {
   return {
     restrict: 'E',
     replace: true,
@@ -8,15 +8,30 @@ angular.module('dashboard').directive("navigation", [function() {
     templateUrl: '/dashboard/templates/navigation.html'
   };
 }]);
-angular.module('dashboard').directive('onFinishRender', function($timeout) {
+angular.module('dashboard').directive('onFinishRender', function ($timeout) {
   return {
     restrict: 'A',
-    link: function(scope, element, attr) {
+    link: function (scope, element, attr) {
       if (scope.$last === true) {
-        $timeout(function() {
+        $timeout(function () {
           scope.$emit('ngRepeatFinished');
         });
       }
     }
-  }
+  };
+});
+angular.module('dashboard').filter('twoDigits', function () {
+  return function (n) {
+    var num = parseInt(n, 10);
+
+    if (isNaN(num)) {
+      return n;
+    }
+
+    if (num / 10 < 1 ) {
+      num = '0' + num;
+    }
+
+    return num;
+  };
 });
