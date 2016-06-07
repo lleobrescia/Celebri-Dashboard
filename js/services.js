4fd9466139e3980e3afab6f983d7e46b60d9508a
Leo Brescia <leo.brescia@pixla.com.br>
2016-06-07 17:32:07 -0300

    retirado comentario

--- js/services.js
+++ js/services.js
@@ -449,20 +449,7 @@ angular.module("dashboard").factory('EstatisticaServ', ['CallAjax', '$q', functi
     getData: getData,
   };
 }]);
-// var recuperarSenha = function (email) {
-//     var urlVar = "http://23.238.16.114/celebri/ServiceCasamento.svc/EnviarEmailRecuperacaoSenha";
-//     var xmlVar = '<EmailCasal xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Email>'.email.'</Email></EmailCasal>';
 
-//     var call = CallAjax.resposta(urlVar, xmlVar);
-//     var deferred = $q.defer();
-
-//     call.success(function (data) {
-//       deferred.resolve(data);
-//     }).error(function () {
-//       deferred.reject(arguments);
-//     });
-//     return deferred.promise;
-//   };
 angular.module("dashboard").factory('AutenticacaoNoivos', ['CallAjax', '$q', function (CallAjax, $q) {
   var autenticar = function (email, senha) {
     var urlVar = "http://23.238.16.114/celebri/ServiceCasamento.svc/AutenticacaoNoivos";
