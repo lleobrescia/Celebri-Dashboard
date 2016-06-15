735ad70abef66639d71a5e65a1eba79e4dfe8bf9
Leo Brescia <leo.brescia@pixla.com.br>
2016-06-15 17:07:59 -0300

    adicionado dados do convite ao padrao do servico

--- js/factory/UserService.js
+++ js/factory/UserService.js
@@ -10,7 +10,28 @@ angular.module("dashboard").factory('UserService', ['$rootScope', function ($roo
     //Dados do Casal
     nomeNoiva: '',
     nomeNoivo: '',
-    dataCasamento: null
+    dataCasamento: null,
+
+    //Dados Convite
+    conviteCheck: false,
+    noiva_mae: '',
+    noiva_mae_memorian: '',
+    noiva_pai: '',
+    noiva_pai_memorian: '',
+    noivo_mae: '',
+    noivo_mae_memorian: '',
+    noivo_pai: '',
+    noivo_pai_memorian: '',
+    cerimonia_cep: '',
+    cerimonia_end: '',
+    cerimonia_numero: '',
+    cerimonia_bairro: '',
+    cerimonia_uf: '',
+    cerimonia_cidade: '',
+    cerimonia_local: '',
+    cerimonia_rota: '',
+    cerimonia_hora: '',
+    cerimonia_min: ''
   };
 
   var service = {
