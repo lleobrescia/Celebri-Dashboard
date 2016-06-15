4d1d7fce497244b75296114f6efaa63ff1f1d20c
Leo Brescia <leo.brescia@pixla.com.br>
2016-06-15 17:07:43 -0300

    confere se o cep tem pelo menos 7 digitos antes de fazer a requisicao

--- js/factory/consultCEP.js
+++ js/factory/consultCEP.js
@@ -4,15 +4,17 @@ angular.module("dashboard").factory('consultCEP', ['CallAjax', '$q', function (C
     var cep = cepToConsult.replace(/\./g, '');
     cep = cep.replace(/\-/g, '');
 
-    var call = $.ajax({ url: "http://api.postmon.com.br/v1/cep/" + cep });
-    var deferred = $q.defer();
+    if (cep.length > 7) {
+      var call = $.ajax({ url: "http://api.postmon.com.br/v1/cep/" + cep });
+      var deferred = $q.defer();
 
-    call.success(function (data) {
-      deferred.resolve(data);
-    }).error(function () {
-      deferred.reject(arguments);
-    });
-    return deferred.promise;
+      call.success(function (data) {
+        deferred.resolve(data);
+      }).error(function () {
+        deferred.reject(arguments);
+      });
+      return deferred.promise;
+    }
   };
 
   return {
