846a78f9d386dcbf348355596083f73a69b16ab3
Leo Brescia <leo.brescia@pixla.com.br>
2016-07-18 11:28:50 -0300

    codigo otimizado para facilitar a leitura

--- js/controller/login.js
+++ js/controller/login.js
@@ -1,102 +1,127 @@
-angular.module("dashboard").controller('login', ['$location', 'ipService', 'ServiceCasamento', 'UserService', '$rootScope', function ($location, ipService, ServiceCasamento, UserService, $rootScope) {
+/**
+ * login.js
+ * templateUrl: "/dashboard/templates/page/login.html"
+ * controllerAs: 'loginCtrl'
+ */
+(function () {
+  'use strict';
+  angular
+    .module("dashboard")
+    .controller('login', Login);
 
-  var self = this;
-  self.recuperarSenha = false;
-  self.carregando = false;
-  self.confirmacao = false;
-  self.nomeUsuario = '';
-  self.senhaUsuario = '';
+  Login.$inject = ['$location', 'ipService', 'ServiceCasamento', 'UserService', '$rootScope'];
 
-  var ID = UserService.ID;
+  function Login($location, ipService, ServiceCasamento, UserService, $rootScope) {
 
-  /**
-   * se existir ID nao ha necessidade de logar
-   */
-  if (ID != null) {
-    $location.path('/dados-do-casal');
-  }
+    var self = this;
+    //ID do usuario. Para verificar se ele ja entrou
+    var ID = UserService.ID;
 
-  self.autenticar = function () {
-    self.carregando = true;
-    self.Result = 'true';
+    self.recuperarSenha = false;//Controla o display da tela de recuperar a senha
+    self.carregando = false;//Controla o display do gif de loading
+    self.confirmacao = false;//Controla o display da tela de confirmação do email enviado
+    self.nomeUsuario = '';//Input nome do usuario
+    self.senhaUsuario = '';//Input senha do usuario
 
-    var urlVar = "http://" + ipService.ip + "/ServiceCasamento.svc/AutenticacaoNoivos";
-    var xmlVar = '<Autenticacao xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Email>' + self.nomeUsuario + '</Email><Senha>' + self.senhaUsuario + '</Senha></Autenticacao>';
+    self.Autenticar = Autenticar;
+    self.EnviarEmail = EnviarEmail;
 
-    ServiceCasamento.SendData(urlVar, xmlVar).then(function (resp) {
-      var respXml = $.parseXML(resp);
-      var check = $(respXml).find('Result').text();
+    /**
+     * Se existir ID nao ha necessidade de logar
+     */
+    if (ID != null) {
+      $location.path('/dados-do-casal');
+    }
 
-      //autenticado
-      if (check == 'true') {
-        //limpa o box de erro, caso houve erro de login anteriormente
-        self.Result = 'true';
-        self.ErrorMessage = "";
+    /**
+     * Funcao para fazer o login.
+     * Envia os dados para o servidor
+     * Armazena os nomes dos noivos, a foto e a senha para o app de teste
+     */
+    function Autenticar() {
+      self.carregando = true;
+      self.result = 'true';
 
-        //armazena o ID
-        UserService.dados.ID = $(respXml).find('Id_usuario_logado').text();
+      var urlVar = "http://" + ipService.ip + "/ServiceCasamento.svc/AutenticacaoNoivos";
+      var xmlVar = '<Autenticacao xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Email>' + self.nomeUsuario + '</Email><Senha>' + self.senhaUsuario + '</Senha></Autenticacao>';
 
-        /**
-         * Verifica qual email esta logando e armazena o nome de acordo.
-         */
-        var emailNoivo = $(respXml).find('EmailNoivo').text();
+      ServiceCasamento.SendData(urlVar, xmlVar).then(function (resp) {
+        var respXml = $.parseXML(resp);
+        var check = $(respXml).find('Result').text();
 
-        if (self.nomeUsuario == emailNoivo) {
-          UserService.dados.nomeUsuario = $(respXml).find('NomeNoivo').text();
-          UserService.dados.senhaApp = $(respXml).find('SenhaNoivoConvidado').text();
-        } else {
-          UserService.dados.nomeUsuario = $(respXml).find('NomeNoiva').text();
-          UserService.dados.senhaApp = $(respXml).find('SenhaNoivaConvidado').text();
+        //Autenticado
+        if (check == 'true') {
+          //limpa o box de erro, caso houve erro de login anteriormente
+          self.result = 'true';
+          self.errorMessage = "";
+
+          //armazena o ID
+          UserService.dados.ID = $(respXml).find('Id_usuario_logado').text();
+
+          // Verifica qual email esta logando e armazena o nome de acordo.
+          var emailNoivo = $(respXml).find('EmailNoivo').text();
+
+          if (self.nomeUsuario == emailNoivo) {
+            UserService.dados.nomeUsuario = $(respXml).find('NomeNoivo').text();
+            UserService.dados.senhaApp = $(respXml).find('SenhaNoivoConvidado').text();
+          } else {
+            UserService.dados.nomeUsuario = $(respXml).find('NomeNoiva').text();
+            UserService.dados.senhaApp = $(respXml).find('SenhaNoivaConvidado').text();
+          }
+
+          //Armazena os nomes dos noivos localmente
+          UserService.dados.nomeNoiva = $(respXml).find('NomeNoiva').text();
+          UserService.dados.nomeNoivo = $(respXml).find('NomeNoivo').text();
+
+          //Armazena a url da foto do casal localmente
+          var imagemFoto = $(respXml).find('Url_foto').text();
+
+          //Verifica a existencia da foto
+          if (imagemFoto == "NULL") {
+            UserService.dados.fotoUrl = 'image/user_login.png';
+          } else {
+            UserService.dados.fotoUrl = $rootScope.fotoCasal = $(respXml).find('Url_foto').text() + "?13:45";
+          }
+
+          //Salva os dados localmente
+          UserService.SaveState();
+
+          //Direciona para a primeira pagina do dashboard
+          $location.path('/dados-do-casal');
         }
 
-        //Armazena os nomes dos noivos localmente
-        UserService.dados.nomeNoiva = $(respXml).find('NomeNoiva').text();
-        UserService.dados.nomeNoivo = $(respXml).find('NomeNoivo').text();
-
-
-        //Armazena a url da foto do casal localmente
-        var imagemFoto = $(respXml).find('Url_foto').text();
-
-        if (imagemFoto == "NULL") {
-          UserService.dados.fotoUrl = 'image/user_login.png';
-        } else {
-          UserService.dados.fotoUrl = $rootScope.fotoCasal = $(respXml).find('Url_foto').text() + "?13:45";
+        //Nao autenticado
+        else {
+          self.carregando = false;
+          //Mostra a mensagem de erro
+          self.result = check;
+          self.errorMessage = $(respXml).find('ErrorMessage').text();
         }
+      });
+    }
 
-        UserService.SaveState();
+    /**
+     * Envia um email, para os noivos, para recuepracao de senha.
+     */
+    function EnviarEmail() {
+      self.carregando = true;
+      self.result = 'true';
 
-        //Direciona para a primeira pagina do dashboard
-        $location.path('/dados-do-casal');
+      var urlVar = "http://" + ipService.ip + "/ServiceCasamento.svc/EnviarEmailRecuperacaoSenha";
+      var xmlVar = '<EmailCasal xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Email>' + self.recEmail + '</Email></EmailCasal>';
 
-      }
-      //nao autenticado
-      else {
+      ServiceCasamento.SendData(urlVar, xmlVar).then(function (resp) {
+        var respXml = $.parseXML(resp);
+        var check = $(respXml).find('Result').text();
+
+        if (check == 'false') {
+          self.errorMessage = "E-mail não encontrado na nossa base de dados.";
+          self.result = check;
+        } else {
+          self.confirmacao = true;
+        }
         self.carregando = false;
-        //Mostra a mensagem de erro
-        self.Result = check;
-        self.ErrorMessage = $(respXml).find('ErrorMessage').text();
-      }
-    });
-  };
-
-  self.enviarEmail = function () {
-    self.carregando = true;
-    self.Result = 'true';
-
-    var urlVar = "http://" + ipService.ip + "/ServiceCasamento.svc/EnviarEmailRecuperacaoSenha";
-    var xmlVar = '<EmailCasal xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Email>' + self.recEmail + '</Email></EmailCasal>';
-
-    ServiceCasamento.SendData(urlVar, xmlVar).then(function (resp) {
-      var respXml = $.parseXML(resp);
-      var check = $(respXml).find('Result').text();
-
-      if (check == 'false') {
-        self.ErrorMessage = "E-mail não encontrado na nossa base de dados.";
-        self.Result = check;
-      } else {
-        self.confirmacao = true;
-      }
-      self.carregando = false;
-    });
-  };
-}]);
+      });
+    }
+  }
+})();
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,80 +1,111 @@
-// INIT
-angular.module("dashboard", ['ngRoute', 'ngFileUpload', 'ngMask', 'rzModule', 'ngAnimate', 'ui.bootstrap', 'chart.js', 'ngImageEditor', 'ngMaterial']);
+// Modules
+angular
+  .module("dashboard", [
+    'ngRoute',//Controlar URL
+    'ngFileUpload',//Usado para upload da foto do casal
+    'ngMask',//Mascara dos inputs
+    'rzModule',//Usado no slider para alterar o tamanho da fonte
+    'ngAnimate',//Usado nas trancições de telas
+    'chart.js',//Usado nas estastisticas
+    'ngImageEditor',//Usado para recortar a foto do casal
+    'ngMaterial'//Usado na data do casamento
+  ]);
 
-angular.module("dashboard").run(['$rootScope', '$location', 'UserService', function ($rootScope, $location, UserService) {
+(function () {
+  'use strict';
 
-  UserService.RestoreState();
+  angular
+    .module("dashboard")
+    .run(Run);
 
-  $rootScope.$on('$routeChangeStart', function (event, next, current) {
-    //hide sidebar on personalizar-convite
-    $rootScope.hideSideBar = true;
+  Run.$inject = ['$rootScope', '$location', 'UserService'];
 
-    if ($location.path() == "/personalizar-convite") {
-      $rootScope.hideSideBar = false;
+  function Run($rootScope, $location, UserService) {
+    //Load os dados do usuario
+    UserService.RestoreState();
+
+    $rootScope.$on('$routeChangeStart', ChangeStart);
+    $rootScope.$on('$routeChangeSuccess', ChangeSuccess);
+
+    //let everthing know that we need to save state now.
+    window.onbeforeunload = function (event) {
+      $rootScope.$broadcast('savestate');
+    };
+
+    function ChangeStart(event, next, current) {
+      //hide sidebar on personalizar-convite
+      $rootScope.hideSideBar = true;
+
+      if ($location.path() == "/personalizar-convite") {
+        $rootScope.hideSideBar = false;
+      }
+      //show loading gif
+      $rootScope.loading = true;
+
+      if (sessionStorage.restorestate == "true") {
+        $rootScope.$broadcast('restorestate'); //let everything know we need to restore state
+        sessionStorage.restorestate = false;
+      }
+
+      var id = UserService.dados.ID;
+      var userAuthenticated = false;
+
+      if (id != null) {
+        userAuthenticated = true;
+      }
+
+      if (!userAuthenticated && !next.isLogin) {
+        $location.path('/login');
+      }
     }
-    //show loading gif
-    $rootScope.loading = true;
 
-    if (sessionStorage.restorestate == "true") {
-      $rootScope.$broadcast('restorestate'); //let everything know we need to restore state
-      sessionStorage.restorestate = false;
+    function ChangeSuccess() {
+      if ($rootScope.fotoCasal === undefined) {
+        $rootScope.fotoCasal = UserService.dados.fotoUrl;
+      }
+      //hide loading gif
+      $rootScope.loading = false;
     }
+  }
+})();
 
-    var id = UserService.dados.ID;
-    var userAuthenticated = false;
+(function () {
+  'use strict';
 
-    if (id != null) {
-      userAuthenticated = true;
-    }
+  angular
+    .module("dashboard")
+    .config(Config);
 
-    if (!userAuthenticated && !next.isLogin) {
-      $location.path('/login');
-    }
-  });
-  $rootScope.$on('$routeChangeSuccess', function () {
+  //Configura aliguagem do calendario
+  function Config($mdDateLocaleProvider) {
+    $mdDateLocaleProvider.months = [
+      'Janeiro', 'Fevereiro', 'Março',
+      'Abril', 'Maio', 'Junho',
+      'Julho', 'Agosto', 'Setembro',
+      'Outubro', 'Novembro', 'Dezembro'
+    ];
 
-    if ($rootScope.fotoCasal === undefined) {
-      $rootScope.fotoCasal = UserService.dados.fotoUrl;
-    }
-
-    //hide loading gif
-    $rootScope.loading = false;
-
-  });
-  //let everthing know that we need to save state now.
-  window.onbeforeunload = function (event) {
-    $rootScope.$broadcast('savestate');
-  };
-}]);
-
-angular.module("dashboard").config(function ($mdDateLocaleProvider) {
-  $mdDateLocaleProvider.months = [
-    'Janeiro', 'Fevereiro', 'Março',
-    'Abril', 'Maio', 'Junho',
-    'Julho', 'Agosto', 'Setembro',
-    'Outubro', 'Novembro', 'Dezembro'
-  ];
-
-  $mdDateLocaleProvider.shortMonths = [
-    'Jan', 'Fev', 'Mar',
-    'Abr', 'Mai', 'Jun',
-    'Jul', 'Ago', 'Set',
-    'Out', 'Nov', 'Dez'
-  ];
-  $mdDateLocaleProvider.days = [
-    'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta',
-    'Sábado', 'Domingo'
-  ];
-  $mdDateLocaleProvider.shortDays = [
-    'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'
-  ];
-  $mdDateLocaleProvider.formatDate = function (date) {
-    return moment(date).format('DD/MM/YYYY');
-  };
-  $mdDateLocaleProvider.parseDate = function (dateString) {
-    var m = moment(dateString, 'DD/MM/YYYY', true);
-    return m.isValid() ? m.toDate() : new Date(NaN);
-  };
-  $mdDateLocaleProvider.msgCalendar = 'Calendario';
-  $mdDateLocaleProvider.msgOpenCalendar = 'Abrir calendario';
-});
+    $mdDateLocaleProvider.shortMonths = [
+      'Jan', 'Fev', 'Mar',
+      'Abr', 'Mai', 'Jun',
+      'Jul', 'Ago', 'Set',
+      'Out', 'Nov', 'Dez'
+    ];
+    $mdDateLocaleProvider.days = [
+      'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta',
+      'Sábado', 'Domingo'
+    ];
+    $mdDateLocaleProvider.shortDays = [
+      'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'
+    ];
+    $mdDateLocaleProvider.formatDate = function (date) {
+      return moment(date).format('DD/MM/YYYY');
+    };
+    $mdDateLocaleProvider.parseDate = function (dateString) {
+      var m = moment(dateString, 'DD/MM/YYYY', true);
+      return m.isValid() ? m.toDate() : new Date(NaN);
+    };
+    $mdDateLocaleProvider.msgCalendar = 'Calendario';
+    $mdDateLocaleProvider.msgOpenCalendar = 'Abrir calendario';
+  }
+})();
