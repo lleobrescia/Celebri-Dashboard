3c5ce31c0bc0d8c8e4a933aec3f95b338b1cf18a
Leo Brescia <leo.brescia@pixla.com.br>
2016-08-09 17:32:32 -0300

    Alteração de dados para teste de homologação

--- php/cieloRequisicao.php
+++ php/cieloRequisicao.php
@@ -13,12 +13,12 @@ date_default_timezone_set('America/Sao_Paulo');
 
 $horaLocal = date('Y-m-d\TH:i:s\.109-03:00');
 
-$url = 'https://qaseCommerce.cielo.com.br/servicos/ecommwsec.do';
+$url = 'https://ecommerce.cielo.com.br/servicos/ecommwsec.do';
 $xml = '<?xml version="1.0" encoding="ISO-8859-1"?>
 <requisicao-transacao id="a97ab62a-7956-41ea-b03f-c2e9f612c293" versao="1.2.1">
   <dados-ec>
-    <numero>1006993069</numero>
-    <chave>25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3</chave>
+    <numero>1078465263</numero>
+    <chave>2148f89a6778fa1f00a77c89d79e4c516ab0e765a37ed5c51c98167e72672880</chave>
   </dados-ec>
 
   <dados-portador>
