5b68c78c1baf1c256dd9925df3a956298edd0bf8
Leo Brescia <leo.brescia@pixla.com.br>
2016-08-23 13:16:35 -0300

    refactoring code

--- js/controller/mainController.js
+++ js/controller/mainController.js
@@ -1,38 +1,52 @@
-angular.module("dashboard").controller('mainController', ['$scope', '$location', 'UserService', '$http', '$window',function ($scope, $location, UserService, $http,$window) {
+(function () {
+  'use strict';
 
-  $scope.checkTemplate = function () {
+  angular
+    .module('dashboard')
+    .controller('MainCtrl', MainCtrl);
 
-    if ("/login" == $location.path()) {
-      $scope.login = "login";
-      $scope.cabecalho = "";
-    } else {
-      $scope.login = "";
-      $scope.cabecalho = "templates/parts/sidebar.html";
+  MainCtrl.$inject = ['$scope', '$location', 'UserService', '$http', '$window'];
+  function MainCtrl($scope, $location, UserService, $http, $window) {
+    var self = this;
+
+    self.CheckTemplate  = CheckTemplate;
+    self.GetTimes       = GetTimes;
+
+    init();
+
+    function init() {
+      if (UserService.listaFonts == null) {
+        //carrega as informações dos blocos(altura,largura, posicao) de cada convite
+        $http.get('data/convites.json')
+          .then(function (res) {
+            try {
+              UserService.dados.listaConvites = res.data;
+            } catch (error) {
+              $window.location.reload();
+            }
+          });
+
+        //carrega as fonts
+        $http.get('data/fonts.json')
+          .then(function (res) {
+            UserService.dados.listaFonts = res.data;
+          });
+        UserService.SaveState();
+      }
     }
-  };
 
-  //for ng-repeat
-  $scope.getTimes = function (n) {
-    return new Array(n);
-  };
+    function GetTimes(n) {
+      return new Array(n);
+    }
 
-  if (UserService.listaFonts == null) {
-    //carrega as informações dos blocos(altura,largura, posicao) de cada convite
-    $http.get('data/convites.json')
-      .then(function (res) {
-        try {
-          UserService.dados.listaConvites = res.data;
-        } catch (error) {
-          $window.location.reload();
-        }
-      });
-
-    //carrega as fonts
-    $http.get('data/fonts.json')
-      .then(function (res) {
-        UserService.dados.listaFonts = res.data;
-      });
-    UserService.SaveState();
+    function CheckTemplate() {
+      if ('/login' === $location.path()) {
+        self.login      = 'login';
+        self.cabecalho  = '';
+      } else {
+        self.login      = '';
+        self.cabecalho  = 'templates/parts/sidebar.html';
+      }
+    }
   }
-
-}]);
+} ());
