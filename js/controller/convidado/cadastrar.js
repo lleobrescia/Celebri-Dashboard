123736bc49fc1334ff6cb1c352d005ad86711e3c
Leo Brescia <leo.brescia@pixla.com.br>
2016-10-26 10:32:44 -0200

    novo local

---
+++ js/controller/convidado/cadastrar.js
@@ -0,0 +1,182 @@
+/**
+ * Configurar Cadastrar Convidados Controller
+ * @namespace Controllers
+ */
+(function () {
+  'use strict';
+  angular
+    .module('dashboard')
+    .controller('CadastrarConvidadosCtrl', CadastrarConvidadosCtrl);
+
+  CadastrarConvidadosCtrl.$inject = ['ServiceCasamento', 'UserService', 'ipService', 'PageService'];
+    /**
+   * @namespace CadastrarConvidadosCtrl
+   * @desc Controla o cadastramento de convidados
+   * @memberOf Controllers
+   */
+  function CadastrarConvidadosCtrl(ServiceCasamento, UserService, ipService, PageService) {
+    var self  = this;
+    var ID    = UserService.dados.ID;
+
+    self.showConvidados         = false;
+    self.convidadoAcompanhantes = '0';
+
+    //Adiciona as funcoes ao escopo
+    self.RemoverConvidado   = RemoverConvidado;
+    self.AdicionarConvidado = AdicionarConvidado;
+
+    Init();
+
+    /**
+     * @name AdicionarConvidado
+     * @desc Adiciona o convidados a lista de convidados e envia os dados ao servidor
+     * @memberOf Controllers.CadastrarConvidadosCtrl
+     */
+    function AdicionarConvidado() {
+      if (self.convidadoNome && self.convidadoEmail) {
+        self.showConvidados = false;
+
+        var urlVar = 'http://' + ipService.ip + '/ServiceCasamento.svc/CadastroConvidados';
+        var xmlVar = '<Convidado xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Email>' + self.convidadoEmail + '</Email><Id>0</Id><Id_usuario_logado>' + ID + '</Id_usuario_logado><Nome>' + self.convidadoNome + '</Nome><Padrinho>false</Padrinho><Qtde_Acompanhantes>' + self.convidadoAcompanhantes + '</Qtde_Acompanhantes><Senha></Senha></Convidado>';
+
+        ServiceCasamento.SendData(urlVar, xmlVar).then(function (resp) {
+          GetConvidados();
+
+          self.convidadoNome          = '';
+          self.convidadoAcompanhantes = '0';
+          self.convidadoEmail         = '';
+        }).catch(function (error) {
+          console.error('AdicionarConvidado -> ', error);
+          console.warn('Dados enviados:', xmlVar);
+        });
+      }
+    }
+
+    /**
+     * @name HandleFile
+     * @desc Lida com planilhas de excel. Pega os dados dos convidados e armazena no servidor
+     * @memberOf Controllers.CadastrarConvidadosCtrl
+     */
+    function HandleFile(e) {
+      var files = e.target.files;
+      var i, f;
+
+      for (i = 0, f = files[i]; i !== files.length; ++i) {
+        var reader  = new FileReader();
+        var name    = f.name;
+
+        reader.onload = function (e) {
+          var data          = e.target.result;
+          var workbook      = XLSX.read(data, { type: 'binary' });
+          var sheetNameList = workbook.SheetNames;
+
+          sheetNameList.forEach(function (y) { /* iterate through sheets */
+            var worksheet = workbook.Sheets[y];
+            var count     = 0;
+            var result    = [];
+
+            for (z in worksheet) {
+
+              /* all keys that do not begin with '!' correspond to cell addresses */
+              if (z[0] === '!') continue;
+              result[count] = worksheet[z].v;
+
+              if (count === 2) {
+                self.showConvidados = false;
+
+                var urlVar = 'http://' + ipService.ip + '/ServiceCasamento.svc/CadastroConvidados';
+                var xmlVar = '<Convidado xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Email>' + result[1] + '</Email><Id>0</Id><Id_usuario_logado>' + ID + '</Id_usuario_logado><Nome>' + result[0] + '</Nome><Padrinho>false</Padrinho><Qtde_Acompanhantes>' + result[2] + '</Qtde_Acompanhantes><Senha></Senha></Convidado>';
+
+                ServiceCasamento.SendData(urlVar, xmlVar).then(function () {
+                  GetConvidados();
+                });
+
+                count   = 0;
+                result  = [];
+              }
+              else count++;
+            }
+          });
+        };
+        reader.readAsBinaryString(f);
+      }
+    }
+
+    /**
+     * @name Init
+     * @desc Setup do controlador
+     * @memberOf Controllers.CadastrarConvidadosCtrl
+     */
+    function Init() {
+      //Set Title 
+      PageService.SetTitle('Cadastrar Convidados');
+      
+      document.getElementById('xlf').addEventListener('change', HandleFile, false);
+
+      //Verifica se ja existe a lista armazenada localmente
+      if (!UserService.dados.convidadoLista) {
+        GetConvidados();
+      } else {
+        self.convidadoLista = UserService.dados.convidadoLista;
+        self.showConvidados = true;
+      }
+    }
+
+    /**
+     * @name GetConvidados
+     * @desc Pega os convidados do servidor
+     * @memberOf Controllers.CadastrarConvidadosCtrl
+     */
+    function GetConvidados() {
+      self.showConvidados = false;
+
+      var urlVar = 'http://' + ipService.ip + '/ServiceCasamento.svc/RetornarConvidados';
+      var xmlVar = '<IdentificaocaoCasal xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Id_casal>' + ID + '</Id_casal></IdentificaocaoCasal>';
+
+      ServiceCasamento.SendData(urlVar, xmlVar).then(function (resp) {
+        var respXml = $.parseXML(resp);
+
+        self.convidadoLista = [];
+
+        $(respXml).find('Convidado').each(function () {
+          self.convidadoLista.push(
+            {
+              'Id'        : $(this).find('Id').text(),
+              'nome'      : $(this).find('Nome').text(),
+              'email'     : $(this).find('Email').text(),
+              'convidados': $(this).find('Qtde_Acompanhantes').text()
+            }
+          );
+        });
+
+        self.showConvidados               = true;
+        UserService.dados.convidadoLista  = self.convidadoLista; //Armazena a lista localmente
+
+        UserService.SaveState();
+      }).catch(function (error) {
+        console.error('GetConvidados -> ', error);
+        console.warn('Dados enviados:', xmlVar);
+      });
+    }
+
+    /**
+     * @name RemoverConvidado
+     * @desc Remove o convidado da lista e apaga os dados do servidor
+     * @memberOf Controllers.CadastrarConvidadosCtrl
+     */
+    function RemoverConvidado(dataId, key) {
+      var urlVar = 'http://' + ipService.ip + '/ServiceCasamento.svc/ExcluirConvidados';
+      var xmlVar = '<ListaRegistrosExcluir xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Id_casal>' + ID + '</Id_casal><Id_registro><int xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays">' + dataId + '</int></Id_registro></ListaRegistrosExcluir>';
+
+      self.convidadoLista.splice(key, 1);
+
+      UserService.dados.convidadoLista = self.convidadoLista;
+      UserService.SaveState();
+
+      ServiceCasamento.SendData(urlVar, xmlVar).catch(function (error) {
+        console.error('RemoverConvidado -> ', error);
+        console.warn('Dados enviados:', xmlVar);
+      });
+    }
+  }
+} ());
---
+++ js/controller/convidado/confirmados.js
@@ -0,0 +1,160 @@
+/**
+ * Configurar Confirmados Controller
+ * @namespace Controllers
+ */
+(function () {
+  'use strict';
+  angular
+    .module('dashboard')
+    .controller('ConfirmadosCtrl', ConfirmadosCtrl);
+
+  ConfirmadosCtrl.$inject = ['UserService', 'ipService', 'ServiceCasamento', '$http', 'EnviarEmail', 'PageService'];
+  /**
+   * @namespace ConfirmadosCtrl
+   * @desc Mostra quem confirmou para a festa
+   * @memberOf Controllers
+   */
+  function ConfirmadosCtrl(UserService, ipService, ServiceCasamento, $http, EnviarEmail, PageService) {
+    var self          = this;
+    var dataCasamento = UserService.dados.dataCasamento;
+    var ID            = UserService.dados.ID;
+    var nomeNoiva     = UserService.dados.nomeNoiva;
+    var nomeNoivo     = UserService.dados.nomeNoivo;
+
+    self.carregando       = true;
+    self.enviando         = false;
+    self.listaConfirmados = [];
+    self.msg              = false;
+    self.total            = 0;
+
+    Init();
+
+  /**
+   * @namespace Download
+   * @desc Fez o dowload em pdf da lista de confirmados
+   * @memberOf Controllers.ConfirmadosCtrl
+   */
+    function Download() {
+      var columns = ['Nome do convidado', 'Acompanhantes', 'Total de Confirmados'];
+      var rows    = [];
+
+      angular.forEach(self.listaConfirmados, function (item) {
+        var acompanhantes = '';
+
+        angular.forEach(item.Acompanhantes, function (itens) {
+          acompanhantes += itens.Nome + '\n';
+        });
+        rows.push(
+          [item.Nome, acompanhantes, item.Total]
+        );
+      });
+
+      var doc = new jsPDF('p', 'pt');
+      doc.autoTable(columns, rows);
+      doc.save('lista_convidados.pdf');
+    }
+
+  /**
+   * @namespace Enviar
+   * @desc Envia para o e-mail fornecido a lista de confirmados
+   * @memberOf Controllers.ConfirmadosCtrl
+   */
+    function Enviar() {
+      if (self.email && self.nome) {
+        self.enviando = true;
+        var data      = dataCasamento.split('/');
+        data          = data[1] + '/' + data[0] + '/' + data[2];
+
+        var destinatario  = self.email;
+        var assunto       = 'Lista de Casamento [Celebri]';
+        var conteudo      = '<span>Olá ' + self.nome + ', <br>essa é a lista dos convidados confirmados para o casamento de ' + nomeNoiva + ' e ' + nomeNoivo + ', no dia ' + data + '</span><br><br><table width="500" cellspacing="0" cellpadding="0" border="1"><thead><tr><td style="padding: 10px;"><b>Nome do convidado</b></td><td style="padding: 10px;"><b>Acompanhantes</b></td><td style="padding: 10px;"><b>Nº de Confirmados <br> (Convidado + Acompanhantes)</b></td></tr></thead>';
+
+        angular.forEach(self.listaConfirmados, function (item) {
+          conteudo += '<tr><td style="padding: 10px;">' + item.Nome + '</td><td style="padding: 10px;">';
+
+          angular.forEach(item.Acompanhantes, function (acompanhant) {
+            conteudo += acompanhant.Nome + '<br>';
+          });
+          conteudo += '</td><td align="center" style="padding: 10px;">' + item.Total + '</td></tr>';
+        });
+
+        conteudo += '</table><span style="margin-top: 30px;display: block;width: 500px;text-align: right;">  Total geral de convidados: <b>' + self.total + '</b></span>';
+
+
+        EnviarEmail.Mail(destinatario, assunto, conteudo).then(function (data) {
+          console.log(data);
+          console.log(conteudo);
+          self.enviando   = false;
+          self.msg        = true;
+          self.nome       = '';
+          self.email      = '';
+        }).catch(function (error) {
+          console.error('Enviar -> ', error);
+        });
+      }
+    }
+
+  /**
+   * @namespace GetConfirmados
+   * @desc Pega a lista de confirmados do servidor
+   * @memberOf Controllers.ConfirmadosCtrl
+   */
+    function GetConfirmados() {
+
+      var urlVar = 'http://' + ipService.ip + '/ServiceCasamento.svc/RetornarConvidadosConfirmados';
+      var xmlVar = '<IdentificaocaoCasal xmlns="http://schemas.datacontract.org/2004/07/WcfServiceCasamento"><Id_casal>' + ID + '</Id_casal></IdentificaocaoCasal>';
+
+      ServiceCasamento.SendData(urlVar, xmlVar).then(function (resp) {
+        var respXml           = $.parseXML(resp);
+        var listaAcompanhante = [];
+        self.listaConfirmados = [];
+
+        $(respXml).find('ListaConvidadosConfirmados').each(function () {
+          var count         = 1;
+          var convidado     = this;
+          listaAcompanhante = [];
+
+          $(convidado).find('ListaAcompanhantes').each(function () {
+            count++;
+            listaAcompanhante.push(
+              {
+                'Nome': $(this).find('Nome').text(),
+              }
+            );
+          });
+
+          self.listaConfirmados.push(
+            {
+              'Nome'          : $(this).find('NomeConvidado').text(),
+              'Acompanhantes' : listaAcompanhante,
+              'Total'         : count
+            }
+          );
+          self.total    += count;
+        });
+        self.carregando = false;
+      }).catch(function (error) {
+        console.error('GetConfirmados -> ', error);
+        console.warn('Dados enviados:', xmlVar);
+      });
+    }
+
+  /**
+   * @namespace Init
+   * @desc Setup do controlador
+   * @memberOf Controllers.ConfirmadosCtrl
+   */
+    function Init() {
+      //Set Title 
+      PageService.SetTitle('Lista de Confirmados');
+
+      /**
+       * Adiciona as funcoes ao scope do controlador
+       */
+      self.Download = Download;
+      self.Enviar   = Enviar;
+
+      GetConfirmados();
+    }
+  }
+} ());
