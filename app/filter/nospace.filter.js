20b16d9692907060b5ce0fd9a686ad4d4a36c026
Leo Brescia <leo.brescia@pixla.com.br>
2016-12-26 17:00:28 -0200

    filtro para retirar espaços

---
+++ app/filter/nospace.filter.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('dashboard')
+    .filter('nospace', NoSpace);
+
+  function NoSpace() {
+    return NoSpaceFilter;
+
+    ////////////////
+
+    function NoSpaceFilter(value) {
+      return (!value) ? '' : value.replace(/ /g, '');
+    }
+  }
+})();
