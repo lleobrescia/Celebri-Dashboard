0366116c1349122641fd8aa5d6f37a87c0cd087e
Leo Brescia <leo.brescia@pixla.com.br>
2017-01-02 17:46:40 -0200

    filtro para gerar dois digitos em numeros

---
+++ app/filter/twoDigits.filter.js
@@ -0,0 +1,35 @@
+/**
+ * Two Digitis Factory
+ * @namespace Filters
+ */
+(function () {
+  'use strict';
+  angular
+    .module('dashboard')
+    .filter('twoDigits', twoDigits);
+
+  /**
+   * @namespace twoDigits
+   * @memberOf Filters
+   */
+  function twoDigits() {
+    return twoDigitsFn;
+
+    /**
+     * @namespace twoDigitsFn
+     * @desc Garante que os min vao ter dois digitos se for menor de 10
+     * @memberOf Filters.twoDigits
+     */
+    function twoDigitsFn(n) {
+      var num = parseInt(n, 10);
+      if (isNaN(num)) {
+        return n;
+      }
+
+      if (num / 10 < 1) {
+        num = '0' + num;
+      }
+      return num;
+    }
+  }
+}());
