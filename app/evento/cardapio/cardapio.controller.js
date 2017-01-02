734fdfa4e64cea21db7b0c4f1d98b7e39e37f19c
Leo Brescia <leo.brescia@pixla.com.br>
2017-01-01 23:55:12 -0200

    cardapio

---
+++ app/evento/cardapio/cardapio.controller.js
@@ -0,0 +1,59 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('dashboard')
+    .controller('CardapioController', CardapioController);
+
+  CardapioController.$inject = ['serverService', 'conversorService', 'ListManagerService'];
+
+  function CardapioController(serverService, conversorService, ListManagerService) {
+    const ID = 1;
+    var vm = this;
+
+    vm.dados = {
+      'Cardapio ': {
+        '@xmlns': 'http://schemas.datacontract.org/2004/07/WcfServiceCasamento',
+        'Descricao': '',
+        'Id': 0,
+        'Id_usuario_logado': ID,
+        'Nome': ''
+      }
+    };
+    vm.cardapios = [];
+
+    vm.Adicionar = Adicionar;
+    vm.Excluir = Excluir;
+
+    Activate();
+
+    ////////////////
+
+    function Activate() {
+      GetDados();
+    }
+
+    function Adicionar() {
+      var dados = conversorService.Json2Xml(vm.dados, '');
+      serverService.Request('CadastrarCardapio', dados).then(function(resp) {
+        GetDados();
+      });
+    }
+
+    function Excluir() {
+
+    }
+
+    function GetDados() {
+      serverService.Get('RetornarCardapio', ID).then(function(resp) {
+        /**
+         * O servico conversorService retorna uma string
+         * O angular converte de string para objeto
+         */
+        resp = angular.fromJson(conversorService.Xml2Json(resp.data, ''));
+        vm.cardapios = resp.ArrayOfCardapio.Cardapio;
+        console.log(resp);
+      });
+    }
+  }
+})();
